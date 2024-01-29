0975d722700e4fe0c2655c65dc53c63d70c1a442
Szczygiel99 <jakub.szczygiel29@gmail.com>
2024-01-29 17:07:35 +0100

    init

---
+++ src/test/java/com/codecool/cctask2024/CcTask2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.cctask2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CcTask2024ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
