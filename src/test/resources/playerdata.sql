a4a7b50a1d086318463dc67d3f3c54df39b3df2e
Szczygiel99 <jakub.szczygiel29@gmail.com>
2024-01-31 16:01:19 +0100

    Test example

--- src/test/resources/playerdata.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO player (id,first_name,last_name,date_of_birth,text_date,degree,sex)
-VALUES (1111,'firstname_test1', 'lastname_test1','2010-05-25','textdate_test1','degree_test1','sex_test1'),
-       (2222,'firstname_test1', 'lastname_test1','2010-05-25','textdate_test1','degree_test1','sex_test1');
